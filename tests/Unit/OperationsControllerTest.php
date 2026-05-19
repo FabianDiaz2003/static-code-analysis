2f3b81824425da7fc00f40c7271abdb9a26fe5c5
MarcusFenix20 <angelitogarcia767@gmail.com>
2026-05-18 19:20:18 -0600

    Primer avance de practica Laravel con analisis estatico

---
+++ tests/Unit/OperationsControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\OperationsController;
+use PHPUnit\Framework\TestCase;
+
+class OperationsControllerTest extends TestCase
+{
+    public function test_addition_result(): void
+    {
+        $controller = new OperationsController;
+
+        $result = $controller->addition(4, 9);
+
+        $this->assertIsInt($result);
+        $this->assertNotNull($result);
+        $this->assertEquals(13, $result);
+    }
+}
